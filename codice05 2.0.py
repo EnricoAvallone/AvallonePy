3167da05fc5855b5af95d9515b6e5e1382a666d0
EnricoAvallone <74616291+EnricoAvallone@users.noreply.github.com>
2020-12-15 19:55:22 +0100

    Add files via upload

---
+++ codice05 2.0.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([5, 6, 9, 12, 14, 16, 17])
+ypoints = np.array([5, 3, 6, 5, 7, 4, 8])
+
+plt.plot(xpoints, ypoints)
+plt.show()
