35dbea6c8e164d7021e56e5484ae81b67aa13c23
EnricoAvallone <74616291+EnricoAvallone@users.noreply.github.com>
2021-05-05 11:02:18 +0200

    Add files via upload

---
+++ Haiku/Haiku_03.py
@@ -0,0 +1,10 @@
+from random import randint as rd
+cinq1=["il giallo grano","la fitta pioggia","Fragole sono:","il gelsomino:","stupendo sole","spente le luci"]
+sett=["tempesta di estate","straripano le sponde","rosse come il fuoco,","alla vista inganna,","nel freddo tramonto","bagliori delle stelle"]
+cinq2=["nel campo muore","arcobaleno","come uomini","odor incanta.","vasto silenzio","cala nel mare","illuminano","cade leggera"]
+str1= cinq1[rd(0,5)]
+str2= sett[rd(0,5)]
+str3= cinq2[rd(0,7)]
+print(str1)
+print(str2)
+print(str3)
