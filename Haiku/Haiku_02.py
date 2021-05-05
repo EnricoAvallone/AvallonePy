73472d9730c2ed3359d9cae0c5232081929f5eb7
EnricoAvallone <74616291+EnricoAvallone@users.noreply.github.com>
2021-05-05 11:01:48 +0200

    Update Haiku_02.py

--- Haiku/Haiku_02.py
+++ Haiku/Haiku_02.py
@@ -2,10 +2,9 @@ from random import randint as rd
 cinq1=["il giallo grano","la fitta pioggia","arcobaleno2","arcobaleno3"]
 sett=["tempesta di estate","straripano le sponde","arcobaleno3","arcobaleno2"]
 cinq2=["nel campo muore","arcobaleno","arcobaleno2","arcobaleno3"]
-for i in range(1):
-    str1= cinq1[rd(0,3)]
-    str2= sett[rd(0,3)]
-    str3= cinq2[rd(0,3)]
+str1= cinq1[rd(0,3)]
+str2= sett[rd(0,3)]
+str3= cinq2[rd(0,3)]
 print(str1)
 print(str2)
 print(str3)
